ff3b435dec9761ed406f3117978f05b036905bf6
behrami <behram1993@gmail.com>
2018-05-16 16:02:10 -0400

    done part 1 of voting

--- election.js
+++ election.js
@@ -2,4 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
 
+  var url = 'https://bb-election-api.herokuapp.com/';
+  var ulTag = document.getElementById('candidates');
+
+  var ajaxFunction = $.ajax({
+                            url: url,
+                            method: 'GET',
+                            dataType: 'json'
+                          });
+
+  ajaxFunction.done(function(responseData){
+
+    var candidatesArray = responseData['candidates'];
+
+    for(i=0; i<candidatesArray.length; i++){
+      var liTag = document.createElement('li');
+      liTag.innerHTML = 'Name: '+ candidatesArray[i].name+"<br> Votes: "+ candidatesArray[i].votes;
+      ulTag.append(liTag);
+      console.log(candidatesArray[i]);
+    }
+
+  });
+
 });
